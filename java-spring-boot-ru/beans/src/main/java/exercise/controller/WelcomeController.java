c9519795ac134d6cf149cd49b4bdf52a1e94bad6
@hexlet/cli <support@hexlet.io>
2025-08-06 20:02:46 +0300

    submit java-spring-boot-ru/beans

---
+++ java-spring-boot-ru/beans/src/main/java/exercise/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package exercise.controller;
+
+import exercise.daytime.Daytime;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// BEGIN
+@RestController
+public class WelcomeController {
+    private final Daytime daytime;
+
+    public WelcomeController(Daytime daytime) {
+        this.daytime = daytime;
+    }
+
+    @GetMapping("/welcome")
+    String welcome() {
+        return "It is " + daytime.getName() + " now! Welcome to Spring!";
+    }
+}
+// END
